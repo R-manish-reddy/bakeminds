edeffe3307d47ae8d429bde3b65a312dd1066976
R-manish-reddy <68364450+R-manish-reddy@users.noreply.github.com>
2021-09-23 00:45:04 +0530

    added questions as comments

--- javascript-challenges/22-9-2021/1 exp checking.js
+++ javascript-challenges/22-9-2021/1 exp checking.js
@@ -1,3 +1,12 @@
+
+// Given an expression string exp, write a function to examine whether the
+// pairs and the orders of “{“, “}”, “(“, “)”, “[“, “]” are correct in exp.
+// ExampleInput: exp = “[()]{}{[()()]()}”
+// Output: Balanced
+// Input: exp = “[(])”
+// Output: Not Balanced
+
+
 var isValid = function(string) {
 
     const map = new Map();
