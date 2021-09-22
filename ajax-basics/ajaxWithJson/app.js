896adfebf90cdf876310d5b41521868956bb8a0b
R-manish-reddy <68364450+R-manish-reddy@users.noreply.github.com>
2021-09-22 19:37:50 +0530

    ajax with JSON object

---
+++ ajax-basics/ajaxWithJson/app.js
@@ -0,0 +1,57 @@
+document.getElementById('button1').addEventListener('click', loadCustomer);
+document.getElementById('button2').addEventListener('click', loadAllCustomers);
+
+function loadCustomer(){
+const xhr = new XMLHttpRequest();
+xhr.open('GET','customer.json',true)
+
+xhr.onload= function(){
+
+    if(this.status === 200){
+        //console.log(this.responseText)
+        const customer = JSON.parse(this.responseText)
+        const output = `
+        <ul>
+        <li>${customer.id}</li>
+        <li>${customer.name}</li>
+        <li>${customer.company}</li>
+        <li>${customer.phone}</li>
+        </ul>
+        `
+        document.getElementById('customer').innerHTML = output;
+    }
+}
+xhr.send();
+
+}
+
+//Load All Customers
+
+function loadAllCustomers(){
+
+const xhr = new XMLHttpRequest();
+xhr.open('GET','customers.json',true)
+xhr.onload = function(){
+
+    if(this.status === 200){
+     const customers = JSON.parse(this.responseText)
+     let output ='';
+     customers.forEach(function(customer){
+        
+        output+= `
+       <ul>
+        <li>ID:${customer.id}</li>
+        <li>Name:${customer.name}</li>
+        <li>Company:${customer.company}</li>
+        <li>Phone:${customer.phone}</li>
+       </ul>
+      `
+       
+      document.getElementById('customers').innerHTML = output
+      })
+      console.log(output)
+    }
+}
+
+xhr.send();
+}
